832c7375868353d865ff404436ff60471e67a90a
nick <npeters2014@knights.ucf.edu>
2016-02-20 14:59:34 -0500

    Moved blob finder to image_converter, added "best circle" to bouyfinder

--- src/bouyfinder.cpp
+++ src/bouyfinder.cpp
@@ -2,116 +2,123 @@
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <features.h>
 
-using namespace cv;
+//using namespace cv;
 
 static const char* OPENCV_WINDOW = "Image window";
 static const std::string CIRCLE_WINDOW = "Circle Window";
 
-    int iLowR = 0;
-    int iHighR = 255;
+int iLowH = 0;
+int iHighH = 179;
 
-    int iLowG = 0;
-    int iHighG = 255;
+int iLowS = 0;
+int iHighS = 255;
 
-    int iLowB = 0;
-    int iHighB = 255;
+int iLowV = 0;
+int iHighV = 255;
 
 class ImageConverter
 {
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
 
 public:
-  ImageConverter()
-    : it_(nh_)
-  {
-
-    //Subscribe to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/usb_cam/image_raw",1,
-                   &ImageConverter::imageCb,this);
-    image_pub_ = it_.advertise("/image_converter/output_video",1);
-
-    cv::namedWindow(OPENCV_WINDOW);
-    namedWindow(CIRCLE_WINDOW);
-    ROS_INFO("wtf?");
-  }
-
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
-
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    cv::Mat imageGrey;
-
-    cvCreateTrackbar("LowR", OPENCV_WINDOW, &iLowR, 255);
-    cvCreateTrackbar("HighR", OPENCV_WINDOW, &iHighR, 255);
-
-    cvCreateTrackbar("LowG", OPENCV_WINDOW, &iLowG, 255);
-    cvCreateTrackbar("HighG", OPENCV_WINDOW, &iHighG, 255);
-
-    cvCreateTrackbar("LowB", OPENCV_WINDOW, &iLowB, 255);
-    cvCreateTrackbar("HighB", OPENCV_WINDOW, &iHighB, 255);
-
-    try
+    ImageConverter()
+        : it_(nh_)
     {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
+        //Subscribe to input video feed and publish output video feed
+        image_sub_ = it_.subscribe("/usb_cam/image_raw",1,
+                                   &ImageConverter::imageCb,this);
+        image_pub_ = it_.advertise("/image_converter/output_video",1);
+
+        cv::namedWindow(OPENCV_WINDOW);
+        cv::namedWindow(CIRCLE_WINDOW);
     }
 
-    cv::cvtColor(cv_ptr->image, imageGrey, CV_BGR2GRAY);
-
-    cv::GaussianBlur(imageGrey, imageGrey, cv::Size(9,9), 2, 2);
-    cv::vector<cv::Vec3f> circles;
-    cv::HoughCircles(imageGrey, circles, CV_HOUGH_GRADIENT,2,imageGrey.rows/4, 200, 100);
-
-    Mat maskedImage = Mat::zeros(cv_ptr->image.rows, cv_ptr->image.cols, CV_8UC3);
-
-    for(size_t i = 0; i < circles.size(); i++)
+    ~ImageConverter()
     {
-      Mat mask;
-      mask = Mat::zeros(imageGrey.rows, imageGrey.cols, CV_8U);
-
-      cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-      int radius = cvRound(circles[i][2]);
-
-      cv::circle(mask, center, radius, cv::Scalar(255),-1,8,0);
-
-      Scalar meanColor;
-      meanColor = mean(cv_ptr->image, mask);
-      ROS_INFO("Found circle with RGB: %f, %f, %f", meanColor[2], meanColor[1], meanColor[0]);
-      
-      if(iLowR < meanColor[2] && meanColor[2] < iHighR && iLowG < meanColor[1] && meanColor[1] < iHighG && iLowB < meanColor[0] && meanColor[0] < iHighB)
-      {
-        cv::circle(cv_ptr->image, center, 3, cv::Scalar(0,255,0), -1, 8, 0);
-        cv::circle(cv_ptr->image, center, radius, cv::Scalar(0,0,255),3,8,0);
-      }
+        cv::destroyWindow(OPENCV_WINDOW);
     }
-    
 
-    //cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    imshow(CIRCLE_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
+    void imageCb(const sensor_msgs::ImageConstPtr& msg)
+    {
+        cv_bridge::CvImagePtr cv_ptr;
+        cv::Mat imageGrey;
 
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
+        cvCreateTrackbar("LowR", OPENCV_WINDOW, &iLowH, 255);
+        cvCreateTrackbar("HighR", OPENCV_WINDOW, &iHighH, 255);
+
+        cvCreateTrackbar("LowG", OPENCV_WINDOW, &iLowS, 255);
+        cvCreateTrackbar("HighG", OPENCV_WINDOW, &iHighS, 255);
+
+        cvCreateTrackbar("LowB", OPENCV_WINDOW, &iLowV, 255);
+        cvCreateTrackbar("HighB", OPENCV_WINDOW, &iHighV, 255);
+
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+
+        cv::cvtColor(cv_ptr->image, imageGrey, CV_BGR2GRAY);
+
+        cv::GaussianBlur(imageGrey, imageGrey, cv::Size(9,9), 2, 2);
+        cv::vector<cv::Vec3f> circles;
+        cv::HoughCircles(imageGrey, circles, CV_HOUGH_GRADIENT,2,imageGrey.rows/4, 200, 100);
+
+        cv::Mat maskedImage = cv::Mat::zeros(cv_ptr->image.rows, cv_ptr->image.cols, CV_8UC3);
+        cv::Mat HSVImage;
+        cv::cvtColor(cv_ptr->image, HSVImage, CV_BGR2HSV);
+
+        int bestHSimilarity = 179;
+        cv::Vec3f bestCircle;
+
+        for(size_t i = 0; i < circles.size(); i++)
+        {
+            cv::Mat mask;
+            mask = cv::Mat::zeros(imageGrey.rows, imageGrey.cols, CV_8U);
+
+            cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+            int radius = cvRound(circles[i][2]);
+
+            cv::circle(mask, center, radius, cv::Scalar(255),-1,8,0);
+
+            cv::Scalar meanColor;
+            meanColor = mean(HSVImage, mask);
+
+            if(iLowH < meanColor[0] && meanColor[0] < iHighH && iLowS < meanColor[1] && meanColor[1] < iHighS && iLowV < meanColor[2] && meanColor[2] < iHighV)
+            {
+                cv::circle(cv_ptr->image, center, 3, cv::Scalar(0,255,0), -1, 8, 0);
+                cv::circle(cv_ptr->image, center, radius, cv::Scalar(0,0,255),3,8,0);
+                if(std::abs(meanColor[0]-(iLowH+iHighH)/2)<bestHSimilarity)
+                {
+                    bestHSimilarity = std::abs(meanColor[0]-(iLowH+iHighH)/2);
+                    bestCircle = circles[i];
+                }
+            }
+        }
+
+        ROS_INFO("Found best circle with X, Y, Size, H: %f, %f, %f, %f", bestCircle[0], bestCircle[1], bestCircle[2], bestHSimilarity);
+        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+        imshow(CIRCLE_WINDOW, maskedImage);
+        cv::waitKey(3);
+
+        image_pub_.publish(cv_ptr->toImageMsg());
+    }
 };
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
-  return 0;
+    ros::init(argc, argv, "image_converter");
+    ImageConverter ic;
+    ros::spin();
+    return 0;
 }
--- src/image_converter.cpp
+++ src/image_converter.cpp
@@ -10,72 +10,101 @@ static const std::string EDGE_WINDOW = "Edge Window";
 
 class ImageConverter
 {
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+    ros::NodeHandle nh_;
+    image_transport::ImageTransport it_;
+    image_transport::Subscriber image_sub_;
+    image_transport::Publisher image_pub_;
 
 public:
-  ImageConverter()
-    : it_(nh_)
-  {
+    ImageConverter()
+        : it_(nh_)
+    {
 
-    //Subscribe to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/usb_cam/image_raw",1,
-			       &ImageConverter::imageCb,this);
-    image_pub_ = it_.advertise("/image_converter/output_video",1);
+        //Subscribe to input video feed and publish output video feed
+        image_sub_ = it_.subscribe("/usb_cam/image_raw",1,
+                                   &ImageConverter::imageCb,this);
+        image_pub_ = it_.advertise("/image_converter/output_video",1);
 
-    cv::namedWindow(OPENCV_WINDOW);
-    cv::namedWindow(EDGE_WINDOW);
-    ROS_INFO("constructed");
-  }
+        cv::namedWindow(OPENCV_WINDOW);
+        cv::namedWindow(EDGE_WINDOW);
+        ROS_INFO("constructed");
+    }
 
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
+    ~ImageConverter()
+    {
+        cv::destroyWindow(OPENCV_WINDOW);
+    }
 
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    cv::Mat gray;
-    cv::Mat edges;
-    try
-      {
-	cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-      }
-    catch (cv_bridge::Exception& e)
-      {
-	ROS_ERROR("cv_bridge exception: %s", e.what());
-	return;
-      }
-    //if(cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-    //  cv::circle(cv_ptr->image, cv::Point(50,50), 10, CV_RGB(255,0,0));
-    
-    cv::cvtColor(cv_ptr->image, gray, CV_BGR2GRAY);
-    cv::GaussianBlur(gray, gray, cv::Size(9,9), 2, 2);
-    cv::vector<cv::Vec3f> circles;
-    cv::HoughCircles(gray, circles, CV_HOUGH_GRADIENT,2,gray.rows/4, 100, 100);
-    for(size_t i = 0; i < circles.size(); i++)
-      {
-	cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
-	int radius = cvRound(circles[i][2]);
-	cv::circle(cv_ptr->image, center, 3, cv::Scalar(0,255,0), -1, 8, 0);
-	cv::circle(cv_ptr->image, center, radius, cv::Scalar(0,0,255),3,8,0);
-      }
-    cv::Canny(gray, edges, 100, 50);
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::imshow(EDGE_WINDOW, edges);
-    cv::waitKey(3);
+    void imageCb(const sensor_msgs::ImageConstPtr& msg)
+    {
+        cv_bridge::CvImagePtr cv_ptr;
+        cv::Mat gray;
+        cv::Mat edges;
+        try
+        {
+            cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+        }
+        catch (cv_bridge::Exception& e)
+        {
+            ROS_ERROR("cv_bridge exception: %s", e.what());
+            return;
+        }
+        //if(cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+        //  cv::circle(cv_ptr->image, cv::Point(50,50), 10, CV_RGB(255,0,0));
+        cv::SimpleBlobDetector::Params params;
 
-    image_pub_.publish(cv_ptr->toImageMsg());
-  }
+        // Change thresholds
+        params.minThreshold = 10;
+        params.maxThreshold = 200;
+
+        // Filter by Area.
+        params.filterByArea = true;
+        params.minArea = 500;
+
+        // Filter by Circularity
+        params.filterByCircularity = true;
+        params.minCircularity = 0.1;
+
+        // Filter by Convexity
+        params.filterByConvexity = true;
+        params.minConvexity = 0.87;
+
+        // Filter by Inertia
+        params.filterByInertia = true;
+        params.minInertiaRatio = 0.01;
+
+        cv::SimpleBlobDetector blobDetector(params);
+        std::vector<cv::KeyPoint> keypoints;
+
+        blobDetector.detect(cv_ptr->image, keypoints);
+
+        cv::Mat im_with_keypoints;
+        cv::drawKeypoints( cv_ptr->image, keypoints, im_with_keypoints, cv::Scalar(0,0,255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
+
+        cv::cvtColor(cv_ptr->image, gray, CV_BGR2GRAY);
+        cv::GaussianBlur(gray, gray, cv::Size(9,9), 2, 2);
+        cv::vector<cv::Vec3f> circles;
+        cv::HoughCircles(gray, circles, CV_HOUGH_GRADIENT,2,gray.rows/4, 100, 100);
+        for(size_t i = 0; i < circles.size(); i++)
+        {
+            cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+            int radius = cvRound(circles[i][2]);
+            cv::circle(cv_ptr->image, center, 3, cv::Scalar(0,255,0), -1, 8, 0);
+            cv::circle(cv_ptr->image, center, radius, cv::Scalar(0,0,255),3,8,0);
+        }
+        cv::Canny(gray, edges, 100, 50);
+        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+        cv::imshow(EDGE_WINDOW, edges);
+        cv::waitKey(3);
+
+        image_pub_.publish(cv_ptr->toImageMsg());
+    }
 };
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
-  return 0;
+    ros::init(argc, argv, "image_converter");
+    ImageConverter ic;
+    ros::spin();
+    return 0;
 }
